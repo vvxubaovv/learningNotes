68f9225e5a334c59e20eba4df18964eb88e184aa
徐豹 <xubao@5agame.cn>
2018-09-22 21:22:49 +0800

    javac

---
+++ javac使用/Test.java
@@ -0,0 +1,7 @@
+package xubao.test;
+
+public class Test{
+	public static void main(String[] args){
+		System.out.println("Test");
+	}
+}
---
+++ javac使用/test.sh
@@ -0,0 +1,10 @@
+#参考网址https://www.cnblogs.com/wangpei/p/7084341.html
+
+#简单编译
+#javac Test.java
+
+#生成路径
+#javac -d dest -s source -h head Test.java
+
+#编译详情显示
+javac -verbose Test.java
